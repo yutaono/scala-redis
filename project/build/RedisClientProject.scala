fba5b5ce5b1e728f21848d3c1a35fbbfe3f8762f
Debasish Ghosh <debasish@debasishg.(none)>
2011-05-14 00:24:48 +0530

    now runs on scala 2.9.0

--- project/build/RedisClientProject.scala
+++ project/build/RedisClientProject.scala
@@ -5,26 +5,12 @@ class RedisClientProject(info: ProjectInfo) extends DefaultProject(info)
 {
   // override def useDefaultConfigurations = true
   override def compileOptions = super.compileOptions ++
-    Seq("-deprecation", "-Xcheckinit", "-Xwarninit", "-encoding", "utf8").map(x => CompileOption(x))
+    Seq("-deprecation", "-Xcheckinit", "-encoding", "utf8").map(x => CompileOption(x))
 
 
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
   val scalaToolsReleases = "Scala-Tools Maven2 Releases Repository" at "http://scala-tools.org/repo-releases"
-  val scalatest =
-    buildScalaVersion match {
-      case "2.7.7" => 
-        "org.scalatest" % "scalatest" % "1.0" 
-      case "2.8.0.RC3" =>
-        "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test"
-      case "2.8.0.RC7" =>
-        "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test"
-      case "2.8.0" =>
-        "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test"
-      case "2.8.1" =>
-        "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test"
-      case "2.9.0-SNAPSHOT" =>
-        "org.scalatest" % "scalatest" % "1.4-SNAPSHOT" % "test"
-    }
+  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test"
   val junit = "junit" % "junit" % "4.8.1"
   val log4j = "log4j" % "log4j" % "1.2.16"
   val slf4japi = "org.slf4j" % "slf4j-api" % "1.5.8"
