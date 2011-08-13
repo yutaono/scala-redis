cfdac63052b5fee482c3a62214613aca4af25bb6
Debasish Ghosh <dghosh@acm.org>
2011-08-13 14:19:28 +0530

    fine tuned load test for PoolSpec

--- src/test/scala/com/redis/PoolSpec.scala
+++ src/test/scala/com/redis/PoolSpec.scala
@@ -81,58 +81,14 @@ class PoolSpec extends Spec
     }
   }
 
-  /**
-  import scala.actors._
-  import scala.actors.Actor._
-
-  case class Push(ms: List[String])
-  class Bencher extends Actor {
-    def act = {
-      loop {
-        react {
-          case Push(messages) =>
-            println("in push")
-            val l = leftp(messages)
-            reply(l)
-        }
-      }
-    }
-  }
-
-  describe("pool load test") {
-    it("should distribute work among the clients") {
-      val l = (0 until 5000).map(_.toString).toList
-      val b = new Bencher
-      b.start
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-      b ! Push(l)
-    }
-  }
-  **/
-
   describe("pool load test") {
     it("should distribute work amongst the clients") {
       val l = (0 until 5000).map(_.toString).toList
       val fns: List[List[String] => Option[Int]] = List.fill(100)(leftp)
       val tasks = fns map (fn => scala.actors.Futures.future { fn(l) })
       val results = tasks map (future => future.apply())
-      println(results)
-      // results should equal(List(Some(5000), Some(5000), Some(1000)))
+      println(results.size)
+      results should equal(List.fill(100)(Some(5000)))
     }
   }
 }
